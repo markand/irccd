0d2164fa0f85f3cd3861e94581a211efd4f346f6
David Demelier <markand@malikania.fr>
2016-02-29 14:22:50 +0100

    CMake: fix incorrect CPACK_SOURCE_IGNORE_FILES

--- cmake/IrccdPackage.cmake
+++ cmake/IrccdPackage.cmake
@@ -18,7 +18,7 @@
 
 set(CPACK_SOURCE_PACKAGE_FILE_NAME "irccd-${IRCCD_VERSION}")
 set(CPACK_SOURCE_GENERATOR TXZ ZIP)
-set(CPACK_SOURCE_IGNORE_FILES .hg .hgignore)
+set(CPACK_SOURCE_IGNORE_FILES "/[.]hg")
 
 #
 # Define the binary package name.
