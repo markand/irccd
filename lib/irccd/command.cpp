85d459719f1b1d516746950d5acbe35ebd5aa437
David Demelier <markand@malikania.fr>
2016-04-22 13:06:56 +0200

    Irccd: std::accumulate is in numeric, not algorithm

--- lib/irccd/command.cpp
+++ lib/irccd/command.cpp
@@ -16,8 +16,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <algorithm>
 #include <iomanip>
+#include <numeric>
 #include <sstream>
 
 #include <irccd/logger.hpp>
