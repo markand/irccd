3cdd9cb69a21309260e324a867e8ffb1c60f3abe
David Demelier <markand@malikania.fr>
2016-04-22 18:50:00 +0200

    Irccd: fix usage of stale file

--- lib/irccd/transport-server.cpp
+++ lib/irccd/transport-server.cpp
@@ -16,7 +16,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include "sysconfig.h"
+#include "sysconfig.hpp"
 
 #if !defined(IRCCD_SYSTEM_WINDOWS)
 #  include <cstdio>
