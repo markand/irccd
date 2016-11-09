bf880d2c7ac0ceb1399c7d5cddb006a982210658
David Demelier <markand@malikania.fr>
2016-11-09 20:46:56 +0100

    Irccdctl: bring back server-nick

---
+++ irccdctl/cli-server-nick.hpp
@@ -0,0 +1,53 @@
+/*
+ * cli-server-nick.hpp -- implementation of irccdctl server-nick
+ *
+ * Copyright (c) 2013-2016 David Demelier <markand@malikania.fr>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef IRCCDCTL_CLI_SERVER_NICK_HPP
+#define IRCCDCTL_CLI_SERVER_NICK_HPP
+
+/**
+ * \file cli-server-nick.hpp
+ * \brief Implementation of irccdctl server-nick.
+ */
+
+#include "cli.hpp"
+
+namespace irccd {
+
+namespace cli {
+
+/**
+ * \brief Implementation of irccdctl server-nick.
+ */
+class ServerNickCli : public Cli {
+public:
+    /**
+     * Default constructor.
+     */
+    ServerNickCli();
+
+    /**
+     * \copydoc Cli::exec
+     */
+    void exec(Irccdctl &irccdctl, const std::vector<std::string> &args) override;
+};
+
+} // !cli
+
+} // !irccd
+
+#endif // !IRCCDCTL_CLI_SERVER_NICK_HPP
