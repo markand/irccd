1573d1b6159f1c006afbab94c389da441a7b15f9
David Demelier <markand@malikania.fr>
2021-12-05 12:25:13 +0100

    cmake: include GNUInstallDirs in macro

--- cmake/IrccdDefinePlugin.cmake
+++ cmake/IrccdDefinePlugin.cmake
@@ -16,6 +16,8 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
 
+include(GNUInstallDirs)
+
 function(irccd_define_native_plugin)
 	set(options "")
 	set(oneValueArgs "MAN;NAME")
